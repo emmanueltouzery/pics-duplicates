adeebdc51e96a23d3aacb982ebd158021c89b069
Emmanuel Touzery <etouzery@gmail.com>
2018-12-30 21:27:05 +0100

    compute hashes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import * as fs from "fs";
-import { HashMap, Vector } from "prelude-ts";
+import * as crypto from "crypto";
+import { HashMap, Vector, Future, Function2 } from "prelude-ts";
+
+// Algorithm depends on availability of OpenSSL on platform
+// Other algorithms: 'sha1', 'md5', 'sha256', 'sha512' ...
+const hashAlgorithm = 'sha1';
+const maxConcurrent = 4;
 
 const folder = process.argv[2];
 
@@ -15,6 +21,37 @@ function getFilesAndSubfolders(baseFolder: string): HashMap<string,Vector<string
         .foldLeft(result, (sofar,cur) => sofar.mergeWith(cur, (a,_)=>a));
 }
 
+function hashFile(filename: string): Future<string> {
+    // https://gist.github.com/GuillermoPena/9233069
+    const hasher = crypto.createHash(hashAlgorithm);
+
+    const s = fs.createReadStream(filename)
+    s.on('data', data => hasher.update(data));
+
+    return Future.ofCallback(cb => s.on('end', cb))
+        .map(_ => hasher.digest('hex'));
+}
+
 const filesAndSubfolders = getFilesAndSubfolders(folder);
 console.log(`Found ${filesAndSubfolders.length()} folders containing ${
-    filesAndSubfolders.foldLeft(0, (sofar,cur)=>sofar+cur[1].length())} files`);
+filesAndSubfolders.foldLeft(0, (sofar,cur)=>sofar+cur[1].length())} files`);
+
+function hashFolder(folder: string, files: Vector<string>): Future<HashMap<string, string>> {
+    const hashFileKeepPath = (f: string) => hashFile(folder+"/"+f)
+        .map<[string,string]>(hash => [folder + "/" + f, hash]);
+    return Future.traverse(files, hashFileKeepPath)
+        .map(v => v.toMap(x=>x));
+}
+
+(async () => {
+
+    const folderHashes = await Future.traverse(
+        filesAndSubfolders,
+        Function2.of(hashFolder).tupled(),
+        {maxConcurrent});
+    const fileToHash = folderHashes.foldLeft(
+        HashMap.empty<string,string>(),
+        (sofar,cur) => sofar.mergeWith(cur, (a,_)=>a));
+    fileToHash.forEach(
+        ([fname, hash]) => console.log(`${fname}\t${hash}`));
+})();

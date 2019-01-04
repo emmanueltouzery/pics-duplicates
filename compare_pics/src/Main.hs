f64671b6ffd1245e5d32d98ed44e84c27e83a2f5
Emmanuel Touzery <etouzery@gmail.com>
2019-01-04 20:23:26 +0100

    gonna drive the process from the haskell app after all. start improving it.

--- compare_pics/src/Main.hs
+++ compare_pics/src/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings, OverloadedLabels #-}
+{-# LANGUAGE OverloadedStrings, OverloadedLabels, LambdaCase, ScopedTypeVariables #-}
 
 import System.Environment
 import System.Exit
@@ -6,7 +6,16 @@ import System.Exit
 import Control.Monad
 import Data.Maybe
 import qualified Data.Text as T
+import qualified Data.Text.IO as T
 import Data.Text (Text)
+import qualified Data.Map as Map
+import Data.Map (Map)
+import qualified System.Directory as Dir
+
+import Safe
+
+import Path
+import Path.IO
 
 import qualified GI.Gtk as Gtk
 import qualified GI.GdkPixbuf as Gdk
@@ -14,24 +23,20 @@ import Data.GI.Base
 
 main :: IO ()
 main = do
-  args <- getArgs
-  if length args < 1
-    then do
-      putStrLn "please pass paths to images as parameters"
+  getArgs >>= \case
+    [picsFolder, hashesFile] -> do
+      pics <- parseAbsDir =<< Dir.canonicalizePath picsFolder
+      candidates <- parseAbsFile =<< Dir.canonicalizePath hashesFile
+      handleImages pics candidates
+    otherwise -> do
+      putStrLn "parameters: <pictures folder> <hashes file>"
       exitWith (ExitFailure 1)
-    else handleImages args
-
-folderName :: Text -> Text
-folderName path = case elements of
-      [] -> ""
-      _ -> last $ init elements
-  where elements = T.splitOn "/" path
 
 data ImageInfoType = ImageInfoReference | ImageInfoCandidate deriving (Eq, Show)
 
-imageInfo :: ImageInfoType -> String -> IO Gtk.VBox
+imageInfo :: ImageInfoType -> Path a File -> IO Gtk.VBox
 imageInfo imageInfoType path = do
-  pixbuf <- Gdk.pixbufNewFromFile path
+  pixbuf <- Gdk.pixbufNewFromFile (toFilePath path)
   (pWidth, pHeight) <- (,) <$> get pixbuf #width <*> get pixbuf #height
   scaledPb <- fromJust <$> Gdk.pixbufScaleSimple pixbuf 150 150 Gdk.InterpTypeBilinear
 
@@ -41,28 +46,48 @@ imageInfo imageInfoType path = do
   #add box image
 
   when (imageInfoType == ImageInfoCandidate) $ do
-      let tShow = T.pack . show
-      button <- new Gtk.Button [ #label := folderName (T.pack path) <> " / "
-                                 <> tShow pWidth <> "x" <> tShow pHeight ]
+      let labelMsg = toFilePath (dirname (parent path)) <> " / "
+                     <> show pWidth <> "x" <> show pHeight
+      button <- new Gtk.Button [ #label := T.pack labelMsg ]
       on button #clicked $ do
           set button [ #sensitive := False]
-          putStrLn path
+          putStrLn (toFilePath path)
           exitSuccess
       #add box button
 
   pure box
 
-handleImages :: [String] -> IO ()
-handleImages imgs = do
+getAvailableImages :: Path Abs File -> IO (Map (Path Rel File) [Path Abs File])
+getAvailableImages hashFile = do
+  -- tailDef to remove the first file which has some meta-info
+  fileLines <- tailDef [] <$> T.lines <$> T.readFile (toFilePath hashFile)
+  filenames <- traverse (parseAbsFile . T.unpack) $
+               headDef ""  . T.splitOn "\t"
+               <$> fileLines
+  let pairs = map (\f -> (filename f, f)) filenames
+  pure $ foldr (\(k,v) sofar -> Map.insertWith (++) k [v] sofar) Map.empty pairs
+
+handleImages :: Path Abs Dir -> Path Abs File -> IO ()
+handleImages imgFolder hashesFile = do
+  pics <- snd <$> listDir imgFolder
+  availableImages <- getAvailableImages hashesFile
+  
   Gtk.init Nothing
-  win <- new Gtk.Window [ #title := "Hi there" ]
+  -- win <- new Gtk.Window [ #title := "Hi there" ]
+  win <- new Gtk.Assistant []
   on win #destroy Gtk.mainQuit
 
   grid <- new Gtk.Grid []
-  #add win grid
 
-  #add grid =<< imageInfo ImageInfoReference (head imgs)
-  forM_ (tail imgs) (#add grid <=< imageInfo ImageInfoCandidate)
+  let pic = head pics
+  print $ filename pic
+
+  #add grid =<< imageInfo ImageInfoReference pic
+  let candidates = Map.findWithDefault [] (filename pic) availableImages
+  print candidates
+  forM_ candidates (#add grid <=< imageInfo ImageInfoCandidate)
+
+  #appendPage win grid
 
   #showAll win
   Gtk.main
